de5fb9b1a6397a22d24e6a7c69b5058348e7500b
AnushkaWijerathna <anushkawijerathna1128@gmail.com>
2026-01-17 01:31:57 +0530

    Installed TailWindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
+//React website eke pennanne meka... api hadna hama functional folder ekkma mekta connect wenna one
 import './App.css'
-import ProductCard from './components/productCard.jsx'
 
+
+//Meka thama web site eka...main eke call krnwa
 function App() {
+  return(
+    <div className='border w-150  h-150 bg-gray-400 relative '>
+      <div className='w-125 h-125 bg-yellow-100 flex flex-col items-center justify-center'>
+        
+          <div className="w-25 h-25 bg-blue-500"> 
 
-  return (
-    <>
-      <ProductCard name="Laptop"  price="150,000.00" image= "https://picsum.photos/id/2/367/267"></ProductCard>
+          </div>
+          <div className='w-25 h-25 bg-green-500 fixed left-137.5 top-137.5'> 
 
-      <ProductCard name="Phone"  price="90,000.00" image= "https://picsum.photos/id/160/367/267"></ProductCard>
-    
-    </>
+          </div>
+          <div className='w-25 h-25 bg-red-500'> 
+
+          </div>
+          <div className='w-25 h-25 absolute right-25 bottom-25 bg-purple-500'> 
+          </div>
+
+      </div>
+
+    </div>
   )
 }
 
